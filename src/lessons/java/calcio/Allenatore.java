c418318ece4fc055bb7be6207ae1f84a4abee077
francesca <francipeco36@gmail.com>
2023-08-02 12:04:43 +0200

    Allenatore class made

--- src/lessons/java/calcio/Allenatore.java
+++ src/lessons/java/calcio/Allenatore.java
@@ -1,13 +1,19 @@
 package lessons.java.calcio;
 
-public class Allenatore {
+public class Allenatore extends Persona{
 
     // ATTRIBUTI: strategia (es: difensiva o offensiva)
+    private String strategia;
 
     // COSTRUTTORI
-
+public Allenatore(String nome, int eta, String strategia){
+    super(nome, eta);
+    this.strategia=strategia;
+}
     // GETTER E SETTER
-
+public String getStrategia(){
+    return strategia;
+}
     // METODI
 
 }
