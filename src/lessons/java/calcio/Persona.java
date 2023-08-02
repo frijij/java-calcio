734e294f84f09bd418085564b91bc54c47813e39
francesca <francipeco36@gmail.com>
2023-08-02 11:50:50 +0200

    Persona class made

--- src/lessons/java/calcio/Persona.java
+++ src/lessons/java/calcio/Persona.java
@@ -3,12 +3,23 @@ package lessons.java.calcio;
 public class Persona {
 
     // ATTRIBUTI: nome ed età
+private String nome;
+private int eta;
 
     // COSTRUTTORI
-
+public Persona(String nome, int eta){
+    this.nome=nome;
+    this.eta= eta;
+}
     // GETTER E SETTER
+    public String getNome() {
+    return nome;
+    }
+    public int getEta() {
+        return eta;
+    }
 
-    // METODI
+// METODI
 
 
 }
