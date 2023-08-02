4f16c764d7a032ceca1c4275223131bfdaedcf65
francesca <francipeco36@gmail.com>
2023-08-02 12:21:26 +0200

    Squadra class made

--- src/lessons/java/calcio/Squadra.java
+++ src/lessons/java/calcio/Squadra.java
@@ -1,11 +1,18 @@
 package lessons.java.calcio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Squadra {
 
     // ATTRIBUTI: lista di oggetti di tipo Giocatore e un Allenatore
-
+List<Giocatore> giocatori;
+Allenatore allenatore;
     // COSTRUTTORI
+public Squadra(List<Giocatore> giocatori, Allenatore allenatore){
+    this.giocatori= new ArrayList<>();
 
+}
     // GETTER E SETTER
 
     // METODI
