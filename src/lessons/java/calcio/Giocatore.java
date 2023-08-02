2956b804989787f6c06b12096b2f2c3798aaeb5b
francesca <francipeco36@gmail.com>
2023-08-02 12:00:52 +0200

    Giocatore class made

--- src/lessons/java/calcio/Giocatore.java
+++ src/lessons/java/calcio/Giocatore.java
@@ -1,11 +1,24 @@
 package lessons.java.calcio;
 
-public class Giocatore {
+public class Giocatore extends Persona {
     // ATTRIBUTI: ruolo
-
+private String ruolo;
     // COSTRUTTORI
-
+public Giocatore(String nome, int eta, String ruolo){
+    super(nome, eta);
+    this.ruolo= ruolo;
+}
     // GETTER E SETTER
-
+public String getRuolo(){
+    return ruolo;
+}
     // METODI
+    @Override
+    public String toString() {
+        return "Giocatore{" +
+                "nome: '" + getNome() + '\'' +
+                ", età: " + getEta() +
+                ", ruolo: '" + ruolo + '\'' +
+                '}';
+    }
 }
